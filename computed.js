dab2fb4ee6fc51861e41cce2ed19736ffc82d83e
subhadeep das <dassubhadeep631@gmail.com>
2023-12-26 13:07:55 +0530

    Computed properties file added

---
+++ computed.js
@@ -0,0 +1,14 @@
+const myKey1="name";
+const myKey2="age";
+
+const value1="Subho";
+const value2=23;
+
+const Person={
+    [myKey1]:value1,
+    [myKey2]:value2,
+};
+
+// Person[myKey1]=value1;
+
+console.log(Person);
